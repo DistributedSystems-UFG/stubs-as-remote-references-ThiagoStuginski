c6c22c89ded1fb1efa12b8781a2bbf024cac530e
ThiagoStuginski <thiagostuginski@discente.ufg.br>
2026-05-03 22:57:02 -0300

    Update constRPC.py

--- constRPC.py
+++ constRPC.py
@@ -4,9 +4,11 @@ APPEND   = '3'
 GETVALUE = '4'
 CREATE   = '5'
 STOP     = '6'
-HOSTS    = ''
+
+HOST_SERVER = '34.194.208.5' # IP da Máquina A
+HOST_C1     = '191.121.223.110' # IP da Máquina B
+HOST_C2     = '191.121.223.110' # IP da Máquina C
+
 PORTS   = 50004
 PORTC1  = 50053
 PORTC2  = 50054
-HOSTC1  = ''
-HOSTC2  = ''
